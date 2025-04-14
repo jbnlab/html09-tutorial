824d534f1a8db59ea08a62f27b5ae22008b7f304
James <jbnguyen7@yahoo.com>
2025-04-14 10:37:15 -0400

    Updated to ES6 standards

--- tny_script.js
+++ tny_script.js
@@ -18,25 +18,25 @@ setInterval("runClock()", 1000);
 /* Function to create and run the countdown clock */
 function runClock() {
    /* Store the current date and time */
-   var currentDay = new Date();
-   var dateStr = currentDay.toLocaleDateString();
-   var timeStr = currentDay.toLocaleTimeString();
+   const currentDay = new Date();
+   const dateStr = currentDay.toLocaleDateString();
+   const timeStr = currentDay.toLocaleTimeString();
 
    /* Display the current date and time */
    document.getElementById("dateNow").innerHTML = dateStr + "<br />" + timeStr;
 
    /* Calculate the days until January 1st */
-   var newYear =  new Date("January 1, 2018");
-   var nextYear = currentDay.getFullYear() + 1;
+   const newYear =  new Date("January 1, 2018");
+   const nextYear = currentDay.getFullYear() + 1;
    newYear.setFullYear(nextYear);
-   var daysLeft = (newYear - currentDay)/(1000*60*60*24);
+   const daysLeft = (newYear - currentDay)/(1000*60*60*24);
 
    /* Calculate the hours left in the current day */
-   var hrsLeft = (daysLeft - Math.floor(daysLeft))*24;
+   const hrsLeft = (daysLeft - Math.floor(daysLeft))*24;
 
    /* Calculate the minutes and seconds left in the current hour */
-   var minsLeft = (hrsLeft - Math.floor(hrsLeft))*60;
-   var secsLeft = (minsLeft - Math.floor(minsLeft))*60;
+   const minsLeft = (hrsLeft - Math.floor(hrsLeft))*60;
+   const secsLeft = (minsLeft - Math.floor(minsLeft))*60;
 
    /* Display the time left until New Year's Eve */
    document.getElementById("days").textContent = Math.floor(daysLeft);
